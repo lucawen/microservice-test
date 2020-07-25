86c6471287f146721c8d0030cc459f00b0b19b6d
Luca Albuquerque <lucalacerda1@gmail.com>
2020-07-25 19:14:28 -0300

    Finish python api

--- python-service/app/utils.py
+++ python-service/app/utils.py
@@ -4,6 +4,12 @@ from app.core.config import settings
 
 
 def request_to_rust():
+    """Make a request to microservice
+
+    Returns:
+        None: Something goes wrong with request
+        dict('result'): The result returned from request
+    """
     req = requests.get(settings.microservice_api)
     if req.status_code == 200:
         return req.json()
